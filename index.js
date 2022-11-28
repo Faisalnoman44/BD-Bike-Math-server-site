57b8bcb2bc4120ceff74acd11d034a196a4a9cf9
Faisalnoman44 <mdnoman.mn11@gmail.com>
2022-11-29 00:56:42 +0600

    jwt token and others setup

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const jwt = require('jsonwebtoken')
+
+
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c0svav3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const bikeCollections = client.db('bdBikeMath').collection('bikeCollections');
+        const userCollections = client.db('bdBikeMath').collection('users');
+
+        // bike 
+
+        app.get('/bikes/:brand', async (req, res) => {
+            const brand = req.params.brand
+            const query = { brand: brand }
+            const bikes = await bikeCollections.find(query).toArray();
+            res.send(bikes);
+        })
+
+
+        // jwt 
+
+        app.get('/jwt', async(req, res) =>{
+            const email = req.query.email;
+            const query = { email : email};
+            const user = await userCollections.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1d'})
+                return res.send({accessToken : token});
+            }
+            res.status(403).send({accessToken: ''})
+        })
+
+
+        // users 
+
+        app.get('/users', async(req, res) =>{
+            const query = {};
+            const users = await userCollections.find(query).toArray();
+            res.send(users)
+        })
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollections.insertOne(user);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.log)
+
+
+app.get('/', async (req, res) => {
+    res.send('Final project server is running')
+});
+
+app.listen(port, () => console.log(`Final project is runnig ${port}`))
+
